79cd245ca31bd5fe7fb14229076897bd8b9a718e
Dima Drozdenko <anonimchik920@gmail.com>
2024-08-26 17:17:48 +0300

    Added tests for models and edit tests form

--- restaurant/tests/test_forms.py
+++ restaurant/tests/test_forms.py
@@ -2,15 +2,17 @@ from django.test import TestCase, override_settings
 
 from restaurant.forms import CookCreationForm
 
+
 class FormsTest(TestCase):
     @override_settings(AUTH_PASSWORD_VALIDATORS=[])
     def test_cook_creation_from_with_correct_data(self):
         form_data = {
-            "username": "test",
-            "password1": "Password1test",
-            "password2": "Password2test",
-            "first_name": "test_first",
-            "last_name": "test_last",
+            "username": "new_user",
+            "email": "new_user@example.com",
+            "password1": "79zilivi",
+            "password2": "79zilivi",
+            "first_name": "new_first_name",
+            "last_name": "new_last_name",
             "years_of_experience": 1,
         }
         form = CookCreationForm(data=form_data)
