00ed445bbfe10c7886355b97212a60ebe6eaddd1
Dima Drozdenko <anonimchik920@gmail.com>
2024-08-27 13:31:09 +0300

    Added view for Dish and Cook

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
-import restaurant
-from restaurant.forms import DishTypeSearchForm
+from restaurant.forms import DishTypeSearchForm, DishSearchForm, DishForm, CookSearchForm
 from restaurant.models import Cook, Dish, DishType
 
 
@@ -28,8 +28,8 @@ def index(request):
 
 class DishTypeListView(LoginRequiredMixin, generic.ListView):
     model = DishType
-    context_object_name = 'dish_types'
-    template_name = 'restaurant/dish_type_list.html'
+    context_object_name = 'dish-types'
+    template_name = 'restaurant/dish-type-list.html'
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -48,6 +48,88 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
 
 class DishTypeCreateView(LoginRequiredMixin, generic.CreateView):
     model = DishType
-    fields = ['__all__',]
-    success_url = reverse_lazy("restaurant:dish_type_list")
-    template_name = 'restaurant/dish_type_create.html'
+    fields = "__all__"
+    success_url = reverse_lazy("restaurant:dish-type-list")
+    template_name = 'restaurant/dish-type-form.html'
+
+
+class DishTypeUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = DishType
+    fields = "__all__"
+    success_url = reverse_lazy("restaurant:dish-type-list")
+    template_name = 'restaurant/dish-type-form.html'
+
+
+class DishTypeDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = DishType
+    success_url = reverse_lazy("restaurant:dish-type-list")
+    template_name = 'restaurant/dish-type-confirm-delete.html'
+
+
+class DishListView(LoginRequiredMixin, generic.ListView):
+    model = Dish
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = DishSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Dish.objects.select_related("dish_type").order_by("name")
+        form = DishSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class DishDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Dish
+
+
+class DishCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("restaurant:dish-list")
+
+
+class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Dish
+    form_class = DishForm
+    success_url = reverse_lazy("restaurant:dish-list")
+
+
+class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("restaurant:dish-list")
+
+
+def toggle_assign_to_dish(request, pk):
+    cook = Cook.objects.get(id=request.user.id)
+    if(
+        Cook.objects.get(id=pk) in cook.dishes.all()
+    ):
+        cook.dishes.remove(pk)
+    return HttpResponseRedirect(reverse_lazy("restaurant:dish-list", args=[pk]))
+
+
+class CookListView(LoginRequiredMixin, generic.ListView):
+    model = Cook
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CookListView, self).get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = CookSearchForm(initial={"username": username})
+        return context
+
+    def get_queryset(self):
+        queryset = Cook.objects.all().order_by("username")
+        form = CookSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
+
