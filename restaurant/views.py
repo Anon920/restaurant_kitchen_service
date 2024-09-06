bbc3bea4b27da4062ee060daffab14498851e745
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-06 15:33:35 +0300

    Added new classes Ingredients to views

--- restaurant/views.py
+++ restaurant/views.py
@@ -6,8 +6,8 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 from restaurant.forms import DishTypeSearchForm, DishSearchForm, DishForm, CookSearchForm, CookCreationForm, \
-    CookUpdateForm
-from restaurant.models import Cook, Dish, DishType
+    CookUpdateForm, IngredientSearchForm, IngredientForm
+from restaurant.models import Cook, Dish, DishType, Ingredient
 
 
 @login_required
@@ -106,6 +106,41 @@ class DishUpdateView(LoginRequiredMixin, generic.UpdateView):
 class DishDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Dish
     success_url = reverse_lazy("restaurant:dish-list")
+
+
+class IngredientListView(LoginRequiredMixin, generic.ListView):
+    model = Ingredient
+    paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IngredientListView, self).get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = IngredientSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Ingredient.objects.select_related("ingredient").order_by("name")
+        form = IngredientSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class IngredientCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Ingredient
+    form_class = IngredientForm
+    success_url = reverse_lazy("")
+
+
+class IngredientUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Ingredient
+    form_class = IngredientForm
+    success_url = reverse_lazy("")
+
+
+class IngredientDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Ingredient
+    success_url = reverse_lazy("")
 
 
 @login_required
