769905a7304b09bc9348cae62b8a16b4a90c132e
Dima Drozdenko <anonimchik920@gmail.com>
2024-09-06 15:34:21 +0300

    Added new urls for Ingredients

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -21,6 +21,11 @@ urlpatterns = [
     path("cook/create/", views.CookCreateView.as_view(), name='cook-create'),
     path("cook/<int:pk>/update/", views.CookUpdateView.as_view(), name='cook-update'),
     path("cook/<int:pk>/delete", views.CookDeleteView.as_view(), name='cook-delete'),
+    path("ingredients/", views.IngredientListView.as_view(), name='ingredient-list'),
+    path("ingredients/add/", views.IngredientCreateView.as_view(), name='ingredient-add'),
+    path("ingredients/<int:pk>/update", views.IngredientUpdateView.as_view(), name='ingredient-update'),
+    path("ingredients/<int:pk>/delete", views.IngredientDeleteView.as_view(), name='ingredient-delete'),
+
 ]
 
 app_name = 'restaurant'
